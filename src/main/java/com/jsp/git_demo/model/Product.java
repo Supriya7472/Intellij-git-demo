26d3f5c558c7912ad42d9580517a57c537b482a3
Supriya7472 <supriyashankar7472@gmail.com>
2025-02-15 16:48:36 +0530

    adding product entity

---
+++ src/main/java/com/jsp/git_demo/model/Product.java
@@ -0,0 +1,31 @@
+package com.jsp.git_demo.model;
+
+public class Product {
+    String id;
+    String productName;
+    double price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
